8e18e5bcafd729d488d57b337af71fd9ad20fe53
yyypearl <hor629@naver.com>
2024-08-29 09:09:15 +0900

    design: 기본 레이아웃 및 GlobalStyle, theme 등 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,52 @@
+"use client";
+
+import GlobalStyles from "@/styles/GlobalStyle";
+import { theme } from "@/styles/theme";
+import styled, { ThemeProvider } from "styled-components";
+import StyledJsxRegistry from "./registry";
+import Header from "@/components/Header";
+import VerticalTab from "@/components/VerticalTab";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
+        <meta name="theme-color" content="#796EF2" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body>
+        <StyledJsxRegistry>
+          <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <Column>
+              <Header />
+              <Row>
+                <VerticalTab />
+                {children}
+              </Row>
+            </Column>
+          </ThemeProvider>
+        </StyledJsxRegistry>
+      </body>
     </html>
   );
 }
+
+const Column = styled.div`
+  width: 100vw;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Row = styled.div`
+  display: flex;
+`;
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,56 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`  
+
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+html {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  box-shadow: 0px 0px 64px 0px rgba(30, 41, 59, 0.1);
+
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+  ::-webkit-scrollbar-thumb {
+    height: 20%; /* 스크롤바의 길이 */
+    background: #D5D5D6; /* 스크롤바의 색상 */
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-track {
+    background: #EBEBEB;  /*스크롤바 뒷 배경 색상*/
+    border-radius: 10px;
+  }
+}
+
+body {
+  width: 100%;
+  height: 100%;
+  max-width: 480px;
+  font-family: "Pretendard";
+  white-space: pre-line;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+button {
+  cursor:pointer;
+  border: none;
+  background: transparent;
+}
+
+@font-face {
+    font-family: "Pretendard";
+    src: url("/public/assets/fonts/PretendardVariable.woff2") format('font-woff2'); 
+  }
+`;
+
+export default GlobalStyles;
