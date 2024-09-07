f9bf182b86a54ae01c3c538fc7305fe8c0580555
yyypearl <hor629@naver.com>
2024-09-08 00:52:32 +0900

    file: icon 이미지 및 state 데이터 순서 변경

--- src/constants/state.ts
+++ src/constants/state.ts
@@ -1,22 +1,22 @@
 export const STATE_BOX = [
     {
         id: 0,
+        text: "유예",
+        state: "SUSPENDED",
+    },
+    {
+        id: 1,
         text: "이용 제한",
         state: "RESTRICTED",
     },
     {
-        id: 1,
+        id: 2,
         text: "점수 삭감",
         state: "DOCKED",
     },
     {
-        id: 2,
+        id: 3,
         text: "무시",
         state: "IGNORED",
-    },
-    {
-        id: 3,
-        text: "유예",
-        state: "SUSPENDED",
     },
 ]
